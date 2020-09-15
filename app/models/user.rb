50378ffe83e374b61c0c233357242876b0212452
petite-amelie <pop_ogi0927@icloud.com>
2020-09-15 10:44:40 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   devise :validatable, password_length: 6..128
 
   has_one_attached :avatar
+  has_many :rooms, dependent: :destroy
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
