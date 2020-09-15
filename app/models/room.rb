75ce1d691be588cd8da2bc48bac31e07f8880fda
petite-amelie <pop_ogi0927@icloud.com>
2020-09-15 18:55:27 +0900

    Roomモデルのバリデーション設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,12 @@
 class Room < ApplicationRecord
   belongs_to :user
   has_many :items, dependent: :destroy
+  has_one_attached :room_image
+
+  with_options presence: true do
+    validates :room_name
+    validates :room_introduction
+    validates :genre
+  end
+
 end
