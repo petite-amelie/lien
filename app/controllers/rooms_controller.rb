4bd34da6cc0daa5272c707c94331a733e813f811
petite-amelie <pop_ogi0927@icloud.com>
2020-09-15 18:53:35 +0900

    newとcreateアクションの実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,29 @@
 class RoomsController < ApplicationController
   def index
   end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(
+      :room
+    ).permit(
+      :room_name, :room_introduction, :room_image, genre: []
+    ).merge(
+      user_id: current_user.id
+    )
+  end
 end
