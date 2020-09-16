f98426c84d09bb1428343a3c93ddea2d87f40d39
petite-amelie <pop_ogi0927@icloud.com>
2020-09-16 20:05:38 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :room
   has_one_attached :item_image
+
+  with_options presence: true do
+    validates :item_name, length: { maximum: 20 }
+    validates :item_image
+  end
+  validates :item_introduction, length: { maximum: 100 }
+  validates :price, format: { with: /\A[0-9]+\z/}
 end
