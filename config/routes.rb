d61c187dee89a24e263edf1c535aeca607277e6d
petite-amelie <pop_ogi0927@icloud.com>
2020-09-18 17:22:12 +0900

    リリースのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
+  patch "/rooms/nonrelease/:id", to: "rooms#nonrelease"
+  patch "/rooms/release/:id", to: "rooms#release"
   resources :rooms, only: [:index, :new, :create, :destroy] do
     resources :items
   end
