fc7333f30605bfe0af568a64ecf64ec588b65667
petite-amelie <pop_ogi0927@icloud.com>
2020-09-20 12:26:03 +0900

    likesテーブル作成

---
+++ db/migrate/20200919065045_create_likes.rb
@@ -0,0 +1,9 @@
+class CreateLikes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :likes do |t|
+      t.integer    :user_id, null: false, foreign_key: true
+      t.integer    :room_id, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
