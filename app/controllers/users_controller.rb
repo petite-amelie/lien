e0c14a0712763389c57db141c1a0708923c9e922
petite-amelie <pop_ogi0927@icloud.com>
2020-09-21 20:20:09 +0900

    profileアクションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @rooms = @user.rooms.with_attached_room_image       #ActiveStorageのN+1問題を解決(countの問題も解決したい)
   end
+
+  def profile
+    @user = User.find(params[:id])
+  end
 end
