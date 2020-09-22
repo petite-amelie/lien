a7bb6e3d465d01beff175bcddb89b35e66e2de8d
petite-amelie <pop_ogi0927@icloud.com>
2020-09-22 19:58:04 +0900

    active_hashのアソシエーション作成

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,9 @@
 class Address < ApplicationRecord
+  # ActiveHash用のアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtension
+  belongs_to_active_hash :prefecture
+  # //ActiveHash用のアソシエーション
+  
   belongs_to :user
   belongs_to :order
 end
