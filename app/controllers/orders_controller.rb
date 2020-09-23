b27f3dcd4eb494fbfb9ed587074a03016f356ef6
petite-amelie <pop_ogi0927@icloud.com>
2020-09-23 16:01:16 +0900

    createアクションの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,4 +3,31 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order = AddressOrder.new
   end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order = AddressOrder.new(order_params)
+    if @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:address_order).permit(:token, :postal_code, :prefecture_id, :city, :building_name, :house_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency:'jpy'
+    )
+  end
 end
