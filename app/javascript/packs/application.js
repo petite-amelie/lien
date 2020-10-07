14f951b4853e887195838633569dd1821ed7039d
petite-amelie <pop_ogi0927@icloud.com>
2020-10-07 13:11:21 +0900

    JQueryの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,8 @@ require("channels")
 require("../genre")
 require("../card")
 require("../search")
-require('./preview') 
+require('./preview')
+require('jquery')
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
